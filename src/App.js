0d65d1a3f97f063b346537fd532634fc63553081
Uzair Iqbal <uzair.qq@outlook.com>
2018-12-30 21:48:13 +0500

    Add Customer With Web api done

--- src/App.js
+++ src/App.js
@@ -1,53 +1,91 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  customerAddButton = () => {
-    console.log("button clicked");
+  constructor(props) {
+    super(props);
+    this.state = {
+      customerTypeId: 0,
+      customerName: "",
+      customerAddress: "",
+      customerContact: ""
+    };
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Customer Type Id:" + this.state.customerTypeId);
+    console.log("Customer Name: " + this.state.customerName);
+    console.log("Customer Address: " + this.state.customerAddress);
+    console.log("Customer Contact: " + this.state.customerContact);
+    const customer = {
+      customerTypeId: this.state.customerTypeId,
+      name: this.state.customerName,
+      address: this.state.customerAddress,
+      contact: this.state.customerContact
+    };
+    axios
+      .post(`http://localhost:56996/api/Customer`, { ...customer })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
   };
-  customerUpdateButton = () => {
-    console.log("Update Button Clicked");
-  };
-  customerDeleteButton = () => {
-    console.log("Delete Button Clicked");
-  };
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  }
   render() {
     return (
       <div>
         <h1>Customer Form</h1>
-        <label>Customer Name:</label>
-        <input
-          required
-          placeholder="Enter Customer Name"
-          type="text"
-          name="customerName"
-        />
-        <br />
-        <label>Customer Address:</label>
-        <input
-          type="text"
-          placeholder="Enter Customer Address"
-          name="customerAddress"
-        />
-        <br />
-        <label>Customer Contact:</label>
-        <input
-          type="text"
-          placeholder="Enter Customer Contact"
-          name="customerContact"
-        />
-        <br />
-        <button type="submit" onClick={this.customerAddButton}>
-          Add
-        </button>
-        <br />
-        <button type="submit" onClick={this.customerUpdateButton}>
-          Update
-        </button>
-        <br />
-        <button type="submit" onClick={this.customerDeleteButton}>
-          Delete
-        </button>
+        <form action="post">
+          <label>Customer Type</label>
+          <select
+            name="customerTypeId"
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          >
+            <option value="1">Daily</option>
+            <option value="2">Weekly</option>
+          </select>
+          <br />
+          <label>Customer Name:</label>
+          <input
+            required
+            placeholder="Enter Customer Name"
+            type="text"
+            name="customerName"
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          <br />
+          <label>Customer Address:</label>
+          <input
+            type="text"
+            placeholder="Enter Customer Address"
+            name="customerAddress"
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          <br />
+          <label>Customer Contact:</label>
+          <input
+            type="text"
+            placeholder="Enter Customer Contact"
+            name="customerContact"
+            value={this.state.value}
+            onChange={e => this.handleChange(e)}
+          />
+          <br />
+          <button type="submit" onClick={this.handleSubmit}>
+            Add
+          </button>
+          <br />
+          <button type="submit">Update</button>
+          <br />
+          <button type="submit">Delete</button>
+        </form>
       </div>
     );
   }
