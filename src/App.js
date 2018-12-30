ee2eb4e36532d38aa1234f9871c94f935906f5db
Uzair Iqbal <uzair.qq@outlook.com>
2018-12-30 19:11:56 +0500

    button handling events

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  customerAddButton = () => {
+    console.log("button clicked");
+  };
+  customerUpdateButton = () => {
+    console.log("Update Button Clicked");
+  };
+  customerDeleteButton = () => {
+    console.log("Delete Button Clicked");
+  };
   render() {
     return (
       <div>
         <h1>Customer Form</h1>
         <label>Customer Name:</label>
-        <input type="text" name="customerName" />
+        <input
+          required
+          placeholder="Enter Customer Name"
+          type="text"
+          name="customerName"
+        />
         <br />
         <label>Customer Address:</label>
-        <input type="text" name="customerAddress" />
+        <input
+          type="text"
+          placeholder="Enter Customer Address"
+          name="customerAddress"
+        />
         <br />
         <label>Customer Contact:</label>
-        <input type="text" name="customerContact" />
+        <input
+          type="text"
+          placeholder="Enter Customer Contact"
+          name="customerContact"
+        />
         <br />
-        <button type="submit">Add</button>
+        <button type="submit" onClick={this.customerAddButton}>
+          Add
+        </button>
         <br />
-        <button type="submit">Update</button>
+        <button type="submit" onClick={this.customerUpdateButton}>
+          Update
+        </button>
         <br />
-        <button type="submit">Delete</button>
+        <button type="submit" onClick={this.customerDeleteButton}>
+          Delete
+        </button>
       </div>
     );
   }
