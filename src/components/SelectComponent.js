11a04f39645d2e2f6a8ced8b4c84cc4113905c2c
Uzair Iqbal <uzair.qq@outlook.com>
2019-02-27 23:23:03 +0500

    New Select Component Created

--- src/components/SelectComponent.js
+++ src/components/SelectComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Select } from "reactstrap";
 
-const Select = props => {
+const SelectComponent = props => {
   return (
     <div className="form-group">
       <label for={props.name}> {props.title} </label>
-      <select
+      {/* <select
         id={props.name}
         name={props.name}
         value={props.value}
@@ -21,9 +22,25 @@ const Select = props => {
             </option>
           );
         })}
+      </select> */}
+      <select
+        // id={props.name}
+        // name={props.name}
+        // value={props.value}
+        customersDropDown={props.options}
+        // onChange={props.handleChange}
+        // className="form-control"
+      >
+        {props.customersDropDown.map(function(data, key) {
+          return (
+            <option key={key} value={data.customerId}>
+              {data.customerName}
+            </option>
+          );
+        })}
       </select>
     </div>
   );
 };
 
-export default Select;
+export default SelectComponent;
