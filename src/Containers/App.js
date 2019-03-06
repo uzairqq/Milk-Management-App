a042dcf6a93ae0704a991b6e87cfa8f33da2e0f3
Uzair Iqbal <uzair.qq@outlook.com>
2019-03-06 18:36:11 +0500

    Error When no Path Exist and introduced switch

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../Containers/Home";
 import Customer from "../Containers/Customer";
+import ErrorComponent from "../Components/Error";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/newCustomer" component={Customer} />
-        </div>
+          <Route component={ErrorComponent} />
+        </Switch>
       </BrowserRouter>
     );
   }
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h1>Error: Path Does not Exist</h1>
+    </div>
+  );
+};
+
+export default ErrorComponent;
