eac5609fa00348adf85cdc16679dbe53189ade44
Uzair Iqbal <uzair.qq@outlook.com>
2019-03-06 17:10:11 +0500

    basic and simple routing in app.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,53 +1,20 @@
 import React, { Component } from "react";
-import SideNav, {
-  Toggle,
-  Nav,
-  NavItem,
-  NavIcon,
-  NavText
-} from "@trendmicro/react-sidenav";
+import { BrowserRouter, Route } from "react-router-dom";
 
-// Be sure to include styles at some point, probably during your bootstraping
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+const TestingComponent = () => {
+  return (
+    <div>
+      <h1>New Testing Component</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <SideNav
-          onSelect={selected => {
-            // Add your code here
-          }}
-        >
-          <SideNav.Toggle />
-          <SideNav.Nav defaultSelected="home">
-            <NavItem eventKey="home">
-              <NavIcon>
-                <i
-                  className="fa fa-fw fa-home"
-                  style={{ fontSize: "1.75em" }}
-                />
-              </NavIcon>
-              <NavText>Home</NavText>
-            </NavItem>
-            <NavItem eventKey="charts">
-              <NavIcon>
-                <i
-                  className="fa fa-fw fa-line-chart"
-                  style={{ fontSize: "1.75em" }}
-                />
-              </NavIcon>
-              <NavText>Charts</NavText>
-              <NavItem eventKey="charts/linechart">
-                <NavText>Line Chart</NavText>
-              </NavItem>
-              <NavItem eventKey="charts/barchart">
-                <NavText>Bar Chart</NavText>
-              </NavItem>
-            </NavItem>
-          </SideNav.Nav>
-        </SideNav>
-      </div>
+      <BrowserRouter>
+        <Route path="/new" component={TestingComponent} />
+      </BrowserRouter>
     );
   }
 }
