ced808a449759f0b58b7ef65c3a05bce87d8b938
Uzair Iqbal <uzair.qq@outlook.com>
2019-03-07 02:48:38 +0500

    Few More Addition in Navigation

--- src/Containers/Navigation.js
+++ src/Containers/Navigation.js
@@ -7,12 +7,13 @@ import SideNav, {
   NavText
 } from "@trendmicro/react-sidenav";
 import Customer from "./Customer";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CustomerRates from "./CustomerRates";
-
-// Be sure to include styles at some point, probably during your bootstraping
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 import HomeComponent from "./Home";
+import CustomerSupplied from "./CustomerSupplied";
+import Supplier from "./Supplier";
+import ErrorComponent from "../Components/Error";
 
 const Navigation = () => {
   return (
@@ -21,7 +22,6 @@ const Navigation = () => {
         render={({ location, history }) => (
           <React.Fragment>
             <SideNav
-              onToggle={false}
               onSelect={selected => {
                 const to = "" + selected;
                 if (location.pathname !== to) {
@@ -37,7 +37,7 @@ const Navigation = () => {
                   </NavIcon>
                   <NavText>Home</NavText>
                 </NavItem>
-                <NavItem eventKey="charts">
+                <NavItem eventKey="customer">
                   <NavIcon>
                     <i
                       className="fa fa-fw fa-line-chart"
@@ -51,16 +51,36 @@ const Navigation = () => {
                   <NavItem eventKey="/customerRates">
                     <NavText>Customer Rates</NavText>
                   </NavItem>
+                  <NavItem eventKey="/customerSupplied">
+                    <NavText>Customer Supplied</NavText>
+                  </NavItem>
+                </NavItem>
+                <NavItem eventKey="supplier">
+                  <NavIcon>
+                    <i className="fa fa-truck" style={{ fontSize: "1.75em" }} />
+                  </NavIcon>
+                  <NavText>Supplier</NavText>
+                  <NavItem eventKey="/newSupplier">
+                    <NavText>New Supplier</NavText>
+                  </NavItem>
                 </NavItem>
               </SideNav.Nav>
             </SideNav>
             <main>
-              <Route path="/" exact component={props => <HomeComponent />} />
-              <Route path="/newCustomer" component={props => <Customer />} />
-              <Route
-                path="/customerRates"
-                component={props => <CustomerRates />}
-              />
+              <Switch>
+                <Route path="/" exact component={props => <HomeComponent />} />
+                <Route path="/newCustomer" component={props => <Customer />} />
+                <Route
+                  path="/customerRates"
+                  component={props => <CustomerRates />}
+                />
+                <Route
+                  path="/customerSupplied"
+                  component={props => <CustomerSupplied />}
+                />
+                <Route path="/newSupplier" component={props => <Supplier />} />
+                <Route component={ErrorComponent} />
+              </Switch>
             </main>
           </React.Fragment>
         )}
